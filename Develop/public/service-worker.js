21cc3a57107b9b08f9dff930a8115270eca4afde
Grant Smith <grantnsmith@gmail.com>
2020-07-08 19:22:34 -0700

    Added db.js to create indexDB.

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -1,62 +1,17 @@
 const FILES_TO_CACHE = [
     '/',
     '/index.html',
-    '/favicon.ico',
+    '/index.js',
+    '/db.js',
     '/manifest.webmanifest',
-    '/assets/css/style.css',
-    '/assets/js/loadImages.js',
-    '/assets/images/icons/icon-72x72.png',
-    '/assets/images/icons/icon-96x96.png',
-    '/assets/images/icons/icon-128x128.png',
-    '/assets/images/icons/icon-144x144.png',
-    '/assets/images/icons/icon-152x152.png',
+    '/styles.css',
     '/assets/images/icons/icon-192x192.png',
-    '/assets/images/icons/icon-384x384.png',
     '/assets/images/icons/icon-512x512.png',
-    '/assets/images/1.jpg',
-    '/assets/images/2.jpg',
-    '/assets/images/3.jpg',
-    '/assets/images/4.jpg',
-    '/assets/images/5.jpg',
-    '/assets/images/6.jpg',
-    '/assets/images/7.jpg',
-    '/assets/images/8.jpg',
-    '/assets/images/9.jpg',
-    '/assets/images/10.jpg',
-    '/assets/images/11.jpg',
-    '/assets/images/12.jpg',
-    '/assets/images/13.jpg',
-    '/assets/images/14.jpg',
-    '/assets/images/15.jpg',
-    '/assets/images/16.jpg',
-    '/assets/images/17.jpg',
-    '/assets/images/18.jpg',
-    '/assets/images/19.jpg',
-    '/assets/images/20.jpg',
-    '/assets/images/21.jpg',
-    '/assets/images/22.jpg',
-    '/assets/images/23.jpg',
-    '/assets/images/24.jpg',
-    '/assets/images/25.jpg',
-    '/assets/images/26.jpg',
-    '/assets/images/27.jpg',
-    '/assets/images/28.jpg',
-    '/assets/images/29.jpg',
-    '/assets/images/30.jpg',
-    '/assets/images/31.jpg',
-    '/assets/images/32.jpg',
-    '/assets/images/33.jpg',
-    '/assets/images/34.jpg',
-    '/assets/images/35.jpg',
-    '/assets/images/36.jpg',
-    '/assets/images/37.jpg',
-    '/assets/images/38.jpg'
   ];
   
   const CACHE_NAME = "static-cache-v2";
   const DATA_CACHE_NAME = "data-cache-v1";
   
-  // install
   self.addEventListener("install", function(evt) {
     evt.waitUntil(
       caches.open(CACHE_NAME).then(cache => {
@@ -68,7 +23,6 @@ const FILES_TO_CACHE = [
     self.skipWaiting();
   });
   
-  // activate
   self.addEventListener("activate", function(evt) {
     evt.waitUntil(
       caches.keys().then(keyList => {
@@ -86,14 +40,12 @@ const FILES_TO_CACHE = [
     self.clients.claim();
   });
   
-  // fetch
   self.addEventListener("fetch", function(evt) {
     if (evt.request.url.includes("/api/")) {
       evt.respondWith(
         caches.open(DATA_CACHE_NAME).then(cache => {
           return fetch(evt.request)
             .then(response => {
-              // If the response was good, clone it and store it in the cache.
               if (response.status === 200) {
                 cache.put(evt.request.url, response.clone());
               }
@@ -101,7 +53,6 @@ const FILES_TO_CACHE = [
               return response;
             })
             .catch(err => {
-              // Network request failed, try to get it from the cache.
               return cache.match(evt.request);
             });
         }).catch(err => console.log(err))
