ebbe403650dfdbf3ab6438b3421055152628151b
Grant Smith <grantnsmith@gmail.com>
2020-11-11 14:33:02 -0800

    updated ORMONGO vars in server.js and on heroku.

--- server.js
+++ server.js
@@ -15,13 +15,25 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget"
+// OLD MONGO CONNECTION - mLAB
+// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget"
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false
+// mongoose.connect(MONGODB_URI, {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
+
+// NEW MONGO CONNECTION - OBJECT ROCKET
+
+mongoose.connect(process.env.ORMONGO_URL, {
+  user: process.env.ORMONGO_USER,
+  pass: process.env.ORMONGO_PASS,
 });
-
+mongoose.connection.on("connected", function () {
+  console.log(
+    "Mongoose successfully connected to Db at" + process.env.ORMONGO_URL
+  );
+});
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
